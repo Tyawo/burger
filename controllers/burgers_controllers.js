7ff320a41a8e7a8497d533646df6731b29b46dcf
Tyawo <48452575+Tyawo@users.noreply.github.com>
2019-05-31 07:33:35 -0400

    "update index burger.js"

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -23,18 +23,24 @@ router.post("/", function(req, res) {
     
   res.redirect("/");
   });
-});
+}); 
 
 // Update the burger count
-router.put("/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   burger.updateOne({
     devoured: req.body.devoured
-  }, condition, function() {
-    res.redirect("/");
+  }, condition, function (result) {//must pass result
+    // res.redirect("/");  ##### this is not neccessary
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
   });
 });
 
